da33f18788abe28585713b5e9f7c080a3ee876ae
Eisson Alipio <eisson.alipio@gmail.com>
2023-03-21 13:22:06 -0500

    test: testing app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,16 +3,20 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('Simple working test', () => {
-  it('the title is visible', () => {
+  test('the title is visible', () => {
     render(<App />);
     expect(screen.getByText(/Hello!/i)).toBeInTheDocument();
   });
 
-  it('should increment count on click', async () => {
-    render(<App />);
+  test('should increment count on click', async () => {
+    const app = render(<App />);
+    //app.debug();
+    //expect(app).toBeInTheDocument();
     userEvent.click(screen.getByRole('button'));
     expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument();
     userEvent.click(screen.getByRole('button'));
     expect(await screen.findByText(/count is: 2/i)).toBeInTheDocument();
   });
+
+  test.todo('pending test');
 });
